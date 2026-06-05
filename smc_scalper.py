c1f626afdd8358444f4b4fc4f5ab598dc8bb8433
APEXZINO <ralphsage13@gmail.com>
2026-06-05 18:06:05 +0100

    Update smc_scalper.py

--- smc_scalper.py
+++ smc_scalper.py
@@ -26,7 +26,7 @@ WAT = timezone(timedelta(hours=1))
 # =============================================================================
 @dataclass
 class Config:
-    api_token: str = "33tcEjX9B3LdBJuZWWJ1N"
+    api_token: str = os.environ.get("DERIV_API_TOKEN")
     app_id:    str = "1089"
     symbol:    str = "R_75"
 
